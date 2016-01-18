a410fd7944e751589d2387af770af488695c11b4
James Coslett <james.coslett@gmail.com>
2016-01-18 10:56:27 -0800

    Functions

--- app/js/rockpaperscissors.js
+++ app/js/rockpaperscissors.js
@@ -36,7 +36,7 @@ function getPlayerMove(move) {
 function getComputerMove(move) {
     // Write an expression that operates on a variable called `move`
     // If a `move` has a value, your expression should evaluate to that value.
-    // However, if `move` is not specified / is null, your expression should equal `randomPlay()`.
+    // However, if `move` is not specified / is nulll, your expression should equal `randomPlay()`.
     if(move){
       return move;
     }
