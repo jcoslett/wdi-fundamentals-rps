f598d2f1a6bc6d160f1d87a88b326bc1bffc55e6
James Coslett <james.coslett@gmail.com>
2016-01-18 09:47:30 -0800

    Defined player move functions

--- app/js/rockpaperscissors.js
+++ app/js/rockpaperscissors.js
@@ -25,13 +25,24 @@ function getPlayerMove(move) {
     // Write an expression that operates on a variable called `move`
     // If a `move` has a value, your expression should evaluate to that value.
     // However, if `move` is not specified / is null, your expression should equal `getInput()`.
-    return /* Your Expression */;
+    if(move){
+      return move;
+    }
+    else {
+      getInput();
+    }
 }
 
 function getComputerMove(move) {
     // Write an expression that operates on a variable called `move`
     // If a `move` has a value, your expression should evaluate to that value.
     // However, if `move` is not specified / is null, your expression should equal `randomPlay()`.
+    if(move){
+      return move;
+    }
+    else {
+      randomPlay();
+    }
     return /* Your Expression */;
 }
 
