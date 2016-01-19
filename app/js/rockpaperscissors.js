03509f7a2e8cfd3b0aafe11bb091ed15d14cae60
James Coslett <james.coslett@gmail.com>
2016-01-19 08:59:00 -0800

    Finished rps

--- app/js/rockpaperscissors.js
+++ app/js/rockpaperscissors.js
@@ -43,11 +43,31 @@ function getComputerMove(move) {
     else {
       randomPlay();
     }
-    return /* Your Expression */;
 }
 
 function getWinner(playerMove,computerMove) {
     var winner;
+    if(playerMove == computerMove){
+      winner = "neither";
+    }
+    else if (playerMove == scissors && computerMove == paper){
+      winner = "player";
+    }
+    else if (playerMove == scissors && computerMove == rock){
+      winner = "computer";
+    }
+    else if (playerMove == rock && computerMove == scissors){
+      winner = "player";
+    }
+    else if (playerMove == rock && computerMove == paper){
+      winner = "computer";
+    }
+    else if (playerMove == paper && computerMove == rock){
+      winner = "player";
+    }
+    else if (playerMove == paper && computerMove == scissors){
+      winner = "computer";
+    }
     // Write code that will set winner to either 'player', 'computer', or 'tie' based on the values of playerMove and computerMove.
     // Assume that the only values playerMove and computerMove can have are 'rock', 'paper', and 'scissors'.
     // The rules of the game are that 'rock' beats 'scissors', 'scissors' beats 'paper', and 'paper' beats 'rock'.
@@ -55,12 +75,24 @@ function getWinner(playerMove,computerMove) {
     return winner;
 }
 
+
 function playToFive() {
     console.log("Let's play Rock, Paper, Scissors");
     var playerWins = 0;
     var computerWins = 0;
+    while (playerWins < 5 && computerWins < 5) {
+    getComputerMove();
+    getPlayerMove();
+    var winner = getWinner();
+    if (winner == "computer"){
+      computerWins += 1;
+    }
+    else if (winner == "player"){
+      playerWins += 1;
+    }
+}
     // Write code that plays 'Rock, Paper, Scissors' until either the player or the computer has won five times.
     /* YOUR CODE HERE */
-    return [playerWins, computerWins];
+    console.log("Final score: Player - "playerWins", Computer - " computerWins);
 }
 
